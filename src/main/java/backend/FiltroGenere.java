6086a5e5914ba1e47325d37fee0b0f2eb71e9f07
FPR10 <frff1010@gmail.com>
2025-05-16 19:31:38 +0200

    Implementazione metodo interfaccia

--- src/main/java/backend/FiltroGenere.java
+++ src/main/java/backend/FiltroGenere.java
@@ -1,8 +1,23 @@
 package backend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FiltroGenere implements FiltroStrategyIF{
+
+    private static Genere_Libri genere;
+
+    public FiltroGenere(Genere_Libri genere){
+        this.genere = genere;
+    }
+
     @Override
-    public boolean filtra(Libro l) {
-        return false;
+    public List<Libro> filtra(List<Libro> libri) {
+        List<Libro> ret = new ArrayList<>();
+        for (Libro l : libri){
+            if (l.getGenLib().equals(genere))
+                ret.add(l);
+        }
+        return ret;
     }
 }
