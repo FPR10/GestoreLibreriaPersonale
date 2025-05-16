b67bf85391ec979fc1b8c5a775b9f4140e4381f4
FPR10 <frff1010@gmail.com>
2025-05-17 00:21:37 +0200

    Implementazione metodo salva su csv

---
+++ src/main/java/salvataggio/SaveToCSV.java
@@ -0,0 +1,28 @@
+package main.java.salvataggio;
+
+import main.java.backend.Libro;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class SaveToCSV implements SaveFileStrategyIF{
+
+    private static final String titoloCSV = "Titolo, Autore, ISBN, Genere, Valutazione, Stato lettura";
+
+    @Override
+    public void salva(List<Libro> libreria, String filePath) {
+        try(FileWriter fw = new FileWriter(filePath)){
+            for (Libro l : libreria){
+                fw.append(l.getTitolo()).append(",");
+                fw.append(l.getAutore()).append(",");
+                fw.append(l.getISBN()).append(",");
+                fw.append(l.getGenLib().toString()).append(",");
+                fw.append(l.getValPers().toString()).append(",");
+                fw.append(l.getStatLett().toString()).append("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
