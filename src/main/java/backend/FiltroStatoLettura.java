6b67f76646492e2486e6f564fce46fcfab92a55b
FPR10 <frff1010@gmail.com>
2025-05-16 19:34:01 +0200

    Implementazione metodo interfaccia

--- src/main/java/backend/FiltroStatoLettura.java
+++ src/main/java/backend/FiltroStatoLettura.java
@@ -1,8 +1,23 @@
 package backend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FiltroStatoLettura implements FiltroStrategyIF{
+
+    private static Stato_Lettura sl;
+
+    public FiltroStatoLettura (Stato_Lettura sl){
+        this.sl = sl;
+    }
+
     @Override
-    public boolean filtra(Libro l) {
-        return false;
+    public List<Libro> filtra(List<Libro> libri) {
+        List<Libro> ret = new ArrayList<>();
+        for (Libro l : libri){
+            if (l.getStatLett().equals(sl))
+                ret.add(l);
+        }
+        return ret;
     }
 }
