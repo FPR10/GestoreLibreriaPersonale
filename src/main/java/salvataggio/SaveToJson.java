c7c8ce22be580744f69f71a54034f2ba4d5301d7
FPR10 <frff1010@gmail.com>
2025-05-17 00:22:06 +0200

    Implementazione metodo salva su file json

---
+++ src/main/java/salvataggio/SaveToJson.java
@@ -0,0 +1,22 @@
+package main.java.salvataggio;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import main.java.backend.*;
+
+import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SaveToJson implements SaveFileStrategyIF{
+
+    @Override
+    public void salva(List<Libro> libreria, String filePath) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (FileWriter writer = new FileWriter(filePath)) {
+            gson.toJson(libreria, writer);
+        } catch (IOException e) {
+            System.err.println("Errore durante l'esportazione: " + e.getMessage());
+        }
+    }
+
+}
