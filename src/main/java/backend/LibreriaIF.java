ec92c503f2f589b75ff8be94fa2f363ac4c6ebf3
FPR10 <frff1010@gmail.com>
2025-05-16 18:26:36 +0200

    Aggiunta metodi

--- src/main/java/backend/LibreriaIF.java
+++ src/main/java/backend/LibreriaIF.java
@@ -1,5 +1,21 @@
 package backend;
 
+import java.util.List;
+
 public interface LibreriaIF {
+    //Metodi base
     void aggiungiLibro (Libro l);
+    void rimuoviLibro (Libro l);
+    void modificaLibro (Libro l);
+
+    //Metodi di ricerca
+    Libro ricercaPerTitolo (String titolo);
+    Libro ricercaPerAutore (String autore);
+
+    //Metodi per filtraggio
+    List <Libro> filtraLibri(FiltroStrategy filtro);
+
+    //Metodi per ordinamento
+    List <Libro> ordinaLibri (OrdinamentoStrategy ordinamento);
+
 }
