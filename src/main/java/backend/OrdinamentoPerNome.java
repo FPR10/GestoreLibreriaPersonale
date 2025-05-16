d39987fcdb89e42eac8b8359c9071acdbc16a486
FPR10 <frff1010@gmail.com>
2025-05-16 22:55:14 +0200

    Prima prova di implementazione

---
+++ src/main/java/backend/OrdinamentoPerNome.java
@@ -0,0 +1,11 @@
+package backend;
+
+import java.util.List;
+
+public class OrdinamentoPerNome implements OrdinamentoStrategyIF{
+
+    @Override
+    public List<Libro> ordina(List<Libro> libri) {
+        return List.of();
+    }
+}
