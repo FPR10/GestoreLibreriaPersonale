ed84f74efccb6cf021497baadf2177a0b8bb5b04
FPR10 <frff1010@gmail.com>
2025-05-16 16:39:57 +0200

    Metodo per controllare correttezza ISBN

--- src/main/java/backend/Libro.java
+++ src/main/java/backend/Libro.java
@@ -46,17 +46,26 @@ public class Libro {
             return this;
         }
 
-        public Libro build(){
+        public Libro build() {
             return new Libro(this);
         }
+    }
 
-        private boolean checkISBN (String isbn){
-            if (isbn == null || isbn.length()!=13){
-                return false;
-            }
-            //TODO
+    private static boolean checkISBN (String isbn){
+        if (isbn == null || isbn.length()!=13){
             return false;
         }
+        int sum = 0;
+        for (int i = 0; i< 12; i++){
+            int num = Character.getNumericValue(isbn.charAt(i));
+            if (i % 2==0)
+                sum += num;//posizioni pari hanno peso 1
+            else
+                sum+= num*3; //posizioni dispari hanno peso 3
+        }
+        int checkSum = (10-(sum%10))%10;
+        int lastDigit = Character.getNumericValue(isbn.charAt(12));
+        return checkSum==lastDigit;
     }
 
     private Libro (Builder b){
