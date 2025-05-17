e998a685e0d14703490fe8cb8fb12ced588976a4
FPR10 <frff1010@gmail.com>
2025-05-17 19:43:03 +0200

    Implementazione metodi aggiugi e rimuovi libro

--- src/main/java/backend/Libreria.java
+++ src/main/java/backend/Libreria.java
@@ -4,18 +4,31 @@ import main.java.backend.filtro.FiltroStrategyIF;
 import main.java.backend.libro.Libro;
 import main.java.backend.ordinamento.OrdinamentoStrategyIF;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
-public class Libreria implements LibreriaIF {
+public class Libreria implements LibreriaIF{
+
+    List<Libro> libreria = new LinkedList<>();
 
     @Override
     public void aggiungiLibro(Libro l) {
-
+        if (!libreria.contains(l))
+            libreria.add(l);
+        else
+            System.err.println("Errore libro già inserito !");
     }
 
+
+    /*
+    Supponiamo che il libro che venga passato come argomento del rimuoviLibro sia sicuramente presente nella libreria
+     */
     @Override
     public void rimuoviLibro(Libro l) {
-
+         libreria.remove(l);
     }
 
     @Override
@@ -41,5 +54,11 @@ public class Libreria implements LibreriaIF {
     @Override
     public List<Libro> ordinaLibri(OrdinamentoStrategyIF ordinamento) {
         return List.of();
+    }
+
+
+    @Override
+    public Iterator<Libro> iterator() {
+        return libreria.iterator();
     }
 }
