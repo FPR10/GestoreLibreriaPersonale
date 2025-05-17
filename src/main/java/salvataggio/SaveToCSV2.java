15d22569fddef8940cdd62a7a28be5e4b9692c98
FPR10 <frff1010@gmail.com>
2025-05-17 12:31:03 +0200

    Prova di codice alternativo per salvare su csv

---
+++ src/main/java/salvataggio/SaveToCSV2.java
@@ -0,0 +1,30 @@
+package main.java.salvataggio;
+
+import main.java.backend.libro.Libro;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Formatter;
+import java.util.List;
+
+public class SaveToCSV2 implements SaveFileStrategyIF{
+    private static final String titoloCSV = "Titolo, Autore, ISBN, Genere, Valutazione, Stato lettura";
+
+    @Override
+    public void salva(List<Libro> libreria, String filePath) {
+        try(FileWriter fw = new FileWriter(filePath)){
+            Formatter ft = new Formatter(fw);
+            fw.append(titoloCSV).append("\n");
+            for (Libro l : libreria){
+                fw.append(l.getTitolo()).append(",");
+                fw.append(l.getAutore()).append(",");
+                fw.append(l.getISBN()).append(",");
+                fw.append(l.getGenLib().toString()).append(",");
+                fw.append(l.getValPers().toString()).append(",");
+                fw.append(l.getStatLett().toString()).append("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
