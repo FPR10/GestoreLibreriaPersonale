b9cd97f8e135b577396f1cb61fdcb47917809e48
FPR10 <frff1010@gmail.com>
2025-05-18 12:17:47 +0200

    Aggiunta funzionalità ricerca esaustiva

--- src/main/java/backend/ricerca/RicercaPerAutore.java
+++ src/main/java/backend/ricerca/RicercaPerAutore.java
@@ -1,10 +1,16 @@
 package main.java.backend.ricerca;
-
 import main.java.backend.libro.Libro;
-
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class RicercaPerAutore implements RicercaStrategyIF{
+
+    private final String autore;
+
+    public RicercaPerAutore(String autore) {
+        this.autore = autore;
+    }
 
     @Override
     public List<Libro> ricerca(List<Libro> libri) {
@@ -13,14 +19,16 @@ public class RicercaPerAutore implements RicercaStrategyIF{
             System.err.print("La libreria è vuota !");
         }
         for (Libro elem : libri){
-            if (elem.getAutore().equalsIgnoreCase(autore)){
+            if (cleanString(elem.getAutore()).contains(cleanString(autore))){
                 ret.add(elem);
             }
+
         }
+        return ret;
     }
 
-    private String cleanInput (String autore){
+    private String cleanString (String autore){
         //rimuove caratteri speciali, punteggiature e spazi a inizio e fine della parola
-        return autore.replace("[^a-z0-9àèéòù ]", "").toLowerCase().trim();
+        return autore.toLowerCase().replaceAll("[^a-z0-9àèéòùì ]", "").trim();
     }
 }
