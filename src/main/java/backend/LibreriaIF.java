4b6cef3b518510b1132f8c9f4d7221776396bf2a
FPR10 <frff1010@gmail.com>
2025-05-18 17:37:43 +0200

    Fixed interfaccia

--- src/main/java/backend/LibreriaIF.java
+++ src/main/java/backend/LibreriaIF.java
@@ -12,14 +12,4 @@ public interface LibreriaIF extends Iterable<Libro> {
     void rimuoviLibro (Libro l);
     void modificaLibro (Libro l);
 
-    //Metodi di ricerca
-    Libro ricercaPerTitolo (String titolo);
-    Libro ricercaPerAutore (String autore);
-
-    //Metodi per filtraggio
-    List <Libro> filtraLibri(FiltroStrategyIF filtro);
-
-    //Metodi per ordinamento
-    List <Libro> ordinaLibri (OrdinamentoStrategyIF ordinamento);
-
 }
