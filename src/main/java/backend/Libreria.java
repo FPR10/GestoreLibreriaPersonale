cd62de670f651e5b45b1747c82872ddd60ab9386
FPR10 <frff1010@gmail.com>
2025-05-18 19:49:44 +0200

    Implementazione Libreria concreta

---
+++ src/main/java/backend/Libreria.java
@@ -0,0 +1,27 @@
+package main.java.backend;
+
+import main.java.backend.libro.Libro;
+
+import java.util.Iterator;
+
+public class Libreria implements LibreriaIF {
+    @Override
+    public void aggiungiLibro(Libro l) {
+
+    }
+
+    @Override
+    public void rimuoviLibro(Libro l) {
+
+    }
+
+    @Override
+    public void modificaLibro(Libro l) {
+
+    }
+
+    @Override
+    public Iterator<Libro> iterator() {
+        return null;
+    }
+}
