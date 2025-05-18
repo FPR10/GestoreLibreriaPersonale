ae4aeaebaaafbe57c197ae50ea2c85b3ed3b97e9
FPR10 <frff1010@gmail.com>
2025-05-18 10:25:12 +0200

    Implementazione metodo ricerca

--- src/main/java/backend/ricerca/RicercaPerTitolo.java
+++ src/main/java/backend/ricerca/RicercaPerTitolo.java
@@ -7,6 +7,14 @@ import java.util.List;
 public class RicercaPerTitolo implements RicercaStrategyIF{
     @Override
     public List<Libro> ricerca(List<Libro> libri) {
-        return List.of();
+        List<Libro> ret = new ArrayList<>();
+         if (libri.isEmpty()){
+             System.err.print("Libreria vuota !");
+         }
+         for (Libro elem : libri){
+             if (elem.getTitolo().equalsIgnoreCase(titolo))
+                 ret.add(elem);
+         }
+         return ret;
     }
 }
