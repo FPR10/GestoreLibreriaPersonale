8825990da87acb199854dfc76044907e2d7ed225
FPR10 <frff1010@gmail.com>
2025-05-18 16:57:29 +0200

    Update vari

--- src/main/java/backend/ricerca/RicercaPerTitolo.java
+++ src/main/java/backend/ricerca/RicercaPerTitolo.java
@@ -1,18 +1,25 @@
 package main.java.backend.ricerca;
-
 import main.java.backend.libro.Libro;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class RicercaPerTitolo implements RicercaStrategyIF{
+
+    private final String titolo;
+
+    public RicercaPerTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
     @Override
     public List<Libro> ricerca(List<Libro> libri) {
         List<Libro> ret = new ArrayList<>();
          if (libri.isEmpty()){
              System.err.print("Libreria vuota !");
+             return ret;
          }
          for (Libro elem : libri){
-             if (elem.getTitolo().equalsIgnoreCase(titolo))
+             if (cleanString(elem.getTitolo()).contains(cleanString(titolo)))
                  ret.add(elem);
          }
          return ret;
