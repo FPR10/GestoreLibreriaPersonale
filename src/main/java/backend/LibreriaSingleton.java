f5a7d5117173612d91f81b730b665d8ccc950a93
FPR10 <frff1010@gmail.com>
2025-05-18 20:14:55 +0200

    Implementazione metodi interfaccia

--- src/main/java/backend/LibreriaSingleton.java
+++ src/main/java/backend/LibreriaSingleton.java
@@ -2,28 +2,55 @@ package main.java.backend;
 
 import main.java.backend.libro.Libro;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public enum LibreriaSingleton implements LibreriaIF{
-    ;
+
+    //Istanza static final della classe enum
+    INSTANCE;
+
+    List<Libro> libreria = new ArrayList<>();
 
     @Override
     public void aggiungiLibro(Libro l) {
-
+        if(!libreria.contains(l))
+            libreria.add(l);
     }
 
     @Override
     public void rimuoviLibro(Libro l) {
-
+        libreria.remove(l);
     }
 
     @Override
     public void modificaLibro(Libro l) {
-
+        int indiceLibro = libreria.indexOf(l);
+        if (indiceLibro!=-1)//libro trovato
+            libreria.set(indiceLibro,l);
     }
 
     @Override
     public Iterator<Libro> iterator() {
-        return null;
+        return libreria.iterator();
+    }
+
+
+
+
+    public void prova(){
+        System.out.println("Ok");
+    }
+
+
+    //Come chiamare il Singleton
+    public static void main(String[] args) {
+        LibreriaSingleton ls = LibreriaSingleton.INSTANCE;
+        ls.prova();
+        LibreriaSingleton ls2 = LibreriaSingleton.INSTANCE;
+        ls2.prova();
+        System.out.println(ls==ls2); //true perchè fanno riferimento allo stesso oggetto in memoria
     }
 }
