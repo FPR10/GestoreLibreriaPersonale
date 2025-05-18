48854db8ecbdc5a091ed133e7def06b6b3e49be7
FPR10 <frff1010@gmail.com>
2025-05-18 20:15:25 +0200

    Ridefinizione metodi interfaccia

--- src/main/java/backend/LibreriaIF.java
+++ src/main/java/backend/LibreriaIF.java
@@ -1,10 +1,5 @@
 package main.java.backend;
-
-import main.java.backend.filtro.FiltroStrategyIF;
 import main.java.backend.libro.Libro;
-import main.java.backend.ordinamento.OrdinamentoStrategyIF;
-
-import java.util.List;
 
 public interface LibreriaIF extends Iterable<Libro> {
 
