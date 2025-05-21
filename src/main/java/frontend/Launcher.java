39c389cc053296d673861e98c6c18d1d38e0de12
FPR10 <frff1010@gmail.com>
2025-05-21 13:48:50 +0200

    Fixed main

--- src/main/java/frontend/Launcher.java
+++ src/main/java/frontend/Launcher.java
@@ -1,11 +1,16 @@
 package main.java.frontend;
 
+import main.java.backend.LibreriaSingleton;
+import main.java.controller.Controller;
+
 import javax.swing.*;
 
 public class Launcher {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             GUI frame = new GUI();
+            LibreriaSingleton libreria = LibreriaSingleton.INSTANCE;
+            Controller controller = new Controller(libreria, frame);
             frame.setVisible(true);
         });
     }
