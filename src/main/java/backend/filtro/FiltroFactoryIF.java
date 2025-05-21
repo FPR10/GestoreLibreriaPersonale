827f0fa7eca4a762c2f03fd6aa10f06a7bcb91e3
FPR10 <frff1010@gmail.com>
2025-05-21 18:57:37 +0200

    Implementazione del factory

--- src/main/java/backend/filtro/FiltroFactoryIF.java
+++ src/main/java/backend/filtro/FiltroFactoryIF.java
@@ -2,4 +2,8 @@ package main.java.backend.filtro;
 
 public interface FiltroFactoryIF {
     FiltroStrategyIF creaFiltro(String tipoFiltro);
+
+    default String clearString (String oldString){
+        return oldString.replace(" ", "_");
+    }
 }
---
+++ src/main/java/backend/filtro/NessunFiltro.java
@@ -0,0 +1,13 @@
+package main.java.backend.filtro;
+
+import main.java.backend.LibreriaSingleton;
+import main.java.backend.libro.Libro;
+
+import java.util.List;
+
+public class NessunFiltro implements FiltroStrategyIF{
+    @Override
+    public List<Libro> filtra(LibreriaSingleton l) {
+        return l.getLibreria();
+    }
+}
