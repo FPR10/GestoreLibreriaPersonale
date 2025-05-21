366e23718836f088739b747393c1f72ec90d8c62
FPR10 <frff1010@gmail.com>
2025-05-21 16:41:56 +0200

    Aggiunta parametro valutazione personale

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -4,20 +4,25 @@ import main.java.backend.LibreriaSingleton;
 import main.java.backend.libro.Genere_Libri;
 import main.java.backend.libro.Libro;
 import main.java.backend.libro.Stato_Lettura;
+import main.java.backend.libro.Valutazione_Personale;
+import main.java.backend.salvataggio.SalvaCSV;
+import main.java.backend.salvataggio.SalvaFileStrategyIF;
+import main.java.backend.salvataggio.SalvaJSON;
 import main.java.frontend.FinestraParametriLibro;
 import main.java.frontend.GUI;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
+import java.util.Map;
+import java.util.Set;
 
 
 public class Controller {
     private static LibreriaSingleton libreria;
-    private final GUI grafica;
+
 
     public Controller(LibreriaSingleton impl, GUI grafica) {
-        this.libreria = impl;
-        this.grafica = grafica;
+        libreria = impl;
         grafica.setController(this);
     }
 
@@ -77,7 +82,8 @@ public class Controller {
      */
     public static void SalvaLibro(JTextField campoTitolo, JTextField campoAutoreNome, JTextField campoAutoreCognome,
                                   JTextField campoIsbn, JComboBox<String> campoGenere, JComboBox<String> campoStato,
-                                  String segnapostoTitolo, FinestraParametriLibro finestra, DefaultTableModel modelloTabella) {
+                                  JComboBox<String> campoValutazione,String segnapostoTitolo,
+                                  FinestraParametriLibro finestra, DefaultTableModel modelloTabella) {
         String titolo = campoTitolo.getText();
         String autoreNome = campoAutoreNome.getText();
         String autoreCognome = campoAutoreCognome.getText();
@@ -85,6 +91,8 @@ public class Controller {
         String genere = (String) campoGenere.getSelectedItem();
         String stato = (String) campoStato.getSelectedItem();
         String cleanedStato = stato.replace(" ", "_");
+        String valutazione = (String) campoValutazione.getSelectedItem();
+        String cleanedValutazione = valutazione.replace(" ", "_");
 
         if (titolo.isEmpty() || autoreCognome.isEmpty() || isbn.isEmpty() ||
                 titolo.equals(segnapostoTitolo) || autoreCognome.equals(segnapostoTitolo) || isbn.equals(segnapostoTitolo)) {
@@ -94,29 +102,35 @@ public class Controller {
                     JOptionPane.WARNING_MESSAGE);
             return;
         }
-        JOptionPane.showMessageDialog(null, "Libro salvato!", "Operazione avvenuta con successo", JOptionPane.INFORMATION_MESSAGE);
 
         Libro toAdd = new Libro.Builder(titolo,autoreCognome, isbn)
                 .setAutoreNome(autoreNome)
                 .setGenereLibri(Genere_Libri.valueOf(genere))
                 .setStatoLettura(Stato_Lettura.valueOf(cleanedStato))
+                .setValutazionePersonale(Valutazione_Personale.valueOf(cleanedValutazione))
                 .build();
+
+
+        //Solleva errore e non lo aggiunge
+        if (libreria.contains(toAdd)){
+            JOptionPane.showMessageDialog(null, "Libro già inserito !", "Errore", JOptionPane.WARNING_MESSAGE);
+            return;
+        }
 
         //Aggiunta al backend
         libreria.aggiungiLibro(toAdd);
-
-        //Aggiunta al frontend
         modelloTabella.addRow(new String[]{titolo,autoreCognome+" "+autoreNome,isbn,genere,stato});
+
+        JOptionPane.showMessageDialog(null, "Libro salvato!", "Operazione avvenuta con successo", JOptionPane.INFORMATION_MESSAGE);
 
         finestra.dispose();  //chisura automatica finestra se il libro è stato salvato
     }
 
 
+    //TODO
     public static void modificaLibro(Libro l){
-
     }
 
-    public static void eliminaLibro(Libro l){
 
     public static void eliminaLibro(Map.Entry<Integer,Libro> toDelete, DefaultTableModel modelloTabella){
         libreria.rimuoviLibro(toDelete.getValue());
@@ -127,13 +141,13 @@ public class Controller {
     public static void salvaJSON(){
         SalvaFileStrategyIF sf = new SalvaJSON();
         sf.salva(libreria, "salvataggio.json");
-        JOptionPane.showMessageDialog(null, "Libro salvato in JSON!", "Operazione avvenuta con successo", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Libreria salvata in JSON!", "Operazione avvenuta con successo", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static void salvaCSV(){
-        SalvaFileStrategyIF sf = new SalvaJSON();
-        sf.salva(libreria, "salvataggio.json");
-        JOptionPane.showMessageDialog(null, "Libro salvato in JSON!", "Operazione avvenuta con successo", JOptionPane.INFORMATION_MESSAGE);
+        SalvaFileStrategyIF sf = new SalvaCSV();
+        sf.salva(libreria, "salvataggio.csv");
+        JOptionPane.showMessageDialog(null, "Libreria salvata in CSV!", "Operazione avvenuta con successo", JOptionPane.INFORMATION_MESSAGE);
 
     }
 
