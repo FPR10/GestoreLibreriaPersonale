432cc7ab4222df7abc9b93383f09c95f112cb943
FPR10 <frff1010@gmail.com>
2025-05-21 23:39:53 +0200

    Implementazione metodo interfaccia

---
+++ src/main/java/backend/ordinamento/OrdinamentoFactory.java
@@ -0,0 +1,20 @@
+package main.java.backend.ordinamento;
+
+import main.java.backend.filtro.NessunFiltro;
+import main.java.backend.libro.Stato_Lettura;
+
+public class OrdinamentoFactory implements OrdinamentoFactoryIF {
+
+    @Override
+    public OrdinamentoStrategyIF creaOrdinamento(String tipoOrdinamento) {
+        switch(tipoOrdinamento){
+            case "Ordina per autore" :
+                return new OrdinamentoPerAutore();
+            case "Ordina per titolo":
+                return new OrdinamentoPerTitolo();
+            case "Ordina per valutazione":
+                return new OrdinamentoPerValutazione();
+            default: return new NessunOrdinamento();
+        }
+    }
+}
