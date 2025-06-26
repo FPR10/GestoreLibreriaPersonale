0e7eaa4b57f87d29d452a7ed43b1c2db2b4b6430
FPR10 <frff1010@gmail.com>
2025-06-26 18:14:35 +0200

    Aggiunta estensione file nei parametri del metodo scelta

--- src/main/java/backend/salvataggio/SalvaRipristinaFactory.java
+++ src/main/java/backend/salvataggio/SalvaRipristinaFactory.java
@@ -6,8 +6,7 @@ import org.apache.commons.io.FilenameUtils;
 public class SalvaRipristinaFactory implements SalvaRipristinaFactoryIF{
 
     @Override
-    public SalvaRipristinaStrategyIF scelta(String filePath) {
-        String estensione = FilenameUtils.getExtension(filePath);
+    public SalvaRipristinaStrategyIF scelta(String filePath, String estensione) {
         switch(estensione){
             case "csv": return new SalvaCSV();
             default: return new SalvaJSON();
--- src/main/java/backend/salvataggio/SalvaRipristinaFactoryIF.java
+++ src/main/java/backend/salvataggio/SalvaRipristinaFactoryIF.java
@@ -1,5 +1,5 @@
 package main.java.backend.salvataggio;
 
 public interface SalvaRipristinaFactoryIF {
-    SalvaRipristinaStrategyIF scelta (String filePath);
+    SalvaRipristinaStrategyIF scelta (String filePath, String estensione);
 }
