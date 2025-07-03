3620e2fcab97e8d5112d68934f53669f7f9236ad
FPR10 <frff1010@gmail.com>
2025-07-03 23:22:45 +0200

    Gestione dei libri senza valutazione

--- src/main/java/backend/ordinamento/OrdinamentoPerValutazione.java
+++ src/main/java/backend/ordinamento/OrdinamentoPerValutazione.java
@@ -3,15 +3,36 @@ package main.java.backend.ordinamento;
 import main.java.backend.LibreriaSingleton;
 import main.java.backend.libro.Libro;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class OrdinamentoPerValutazione implements OrdinamentoStrategyIF {
+
     @Override
     public List<Libro> ordina() {
         LibreriaSingleton libri = LibreriaSingleton.INSTANCE;
-        List<Libro>copia = libri.getLibreria();
-        copia.sort(Comparator.comparing(Libro::getValPers).reversed());
-        return copia;
+        List<Libro> copia = libri.getLibreria();
+
+        //Per gestire i libri eventualmente senza valutazione
+        List<Libro> libriConVal = new ArrayList<>();
+        List<Libro> libriSenzaVal = new ArrayList<>();
+
+        for (Libro l : copia) {
+            if (l.getValPers() == null) {
+                libriSenzaVal.add(l);
+            } else {
+                libriConVal.add(l);
+            }
+        }
+        //Ordinamento dei libri con valutazione
+        libriConVal.sort(Comparator.comparing(Libro::getValPers).reversed());
+
+        //Aggiunta dei libri senza valutazione in fondo
+        libriConVal.addAll(libriSenzaVal);
+
+        return libriConVal;
     }
+
 }
+
