36b53ae76f7992a0afac0b92663be27573193d66
FPR10 <frff1010@gmail.com>
2025-07-04 00:31:03 +0200

    Aggiunta metodi di test salvataggio json/csv

--- test/src/SalvataggioTest.java
+++ test/src/SalvataggioTest.java
@@ -1,2 +1,62 @@
+import main.java.backend.LibreriaSingleton;
+import main.java.backend.libro.Libro;
+import main.java.backend.salvataggio.SalvaCSV;
+import main.java.backend.salvataggio.SalvaJSON;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SalvataggioTest {
+
+    static LibreriaSingleton libreria;
+    static LibriEsempio datiInput;
+    static List<Libro> listaLibriEsempio;
+
+    @BeforeAll
+    static void inizializza() {
+        libreria = LibreriaSingleton.INSTANCE;
+        datiInput = new LibriEsempio();
+        datiInput.popolaLibreria();
+        listaLibriEsempio= datiInput.getLibri();
+
+        for (Libro l : listaLibriEsempio){
+            libreria.aggiungiLibro(l);
+        }
+    }
+
+    @Test
+    void salvaRipristinaJson(){
+        Libro l = listaLibriEsempio.getLast();
+
+        SalvaJSON salvJson = new SalvaJSON();
+        salvJson.salva("libri.json");
+        libreria.clear();
+        assertEquals(0, libreria.size());
+        salvJson.ripristina("libri.json");
+
+
+        List<Libro> libriRipristinati = libreria.getLibreria();
+        assertEquals(6, libriRipristinati.size());
+        assertTrue(libriRipristinati.contains(l));
+    }
+
+    @Test
+    void salvaRipristinaCsv(){
+        Libro l = listaLibriEsempio.get(2);
+
+        SalvaCSV salvCSV = new SalvaCSV();
+        salvCSV.salva("libri.csv");
+        libreria.clear();
+        assertEquals(0, libreria.size());
+        salvCSV.ripristina("libri.csv");
+
+
+        List<Libro> libriRipristinati = libreria.getLibreria();
+        assertEquals(6, libriRipristinati.size());
+        assertTrue(libriRipristinati.contains(l));
+    }
+    
 }
