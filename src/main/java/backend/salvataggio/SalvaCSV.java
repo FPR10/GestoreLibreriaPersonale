0180e476b43d9fafc477bf92e72caec2a65cda8c
FPR10 <frff1010@gmail.com>
2025-07-04 00:09:33 +0200

    Gestione valori null

--- src/main/java/backend/salvataggio/SalvaCSV.java
+++ src/main/java/backend/salvataggio/SalvaCSV.java
@@ -21,12 +21,14 @@ public class SalvaCSV implements SalvaRipristinaStrategyIF {
         try(FileWriter fw = new FileWriter(filePath)){
             for (Libro l : libreria.getLibreria()){
                 fw.append(l.getTitolo()).append(",");
-                fw.append(l.getNomeAutore()).append(",");
+                fw.append(l.getNomeAutore()!= null ? l.getNomeAutore() : " ").append(",");
                 fw.append(l.getCognomeAutore()).append(",");
                 fw.append(l.getISBN()).append(",");
-                fw.append(l.getGenLib().toString()).append(",");
-                fw.append(l.getValPers().toString()).append(",");
-                fw.append(l.getStatLett().toString()).append("\n");
+
+                //Append + gestione di eventuali null
+                fw.append(l.getGenLib()!= null ? l.getGenLib().toString() : " ").append(",");
+                fw.append(l.getValPers()!= null ? l.getValPers().toString() : " ").append(",");
+                fw.append(l.getStatLett()!= null ? l.getStatLett().toString() : " ").append("\n");
             }
         } catch (IOException e) {
             System.err.println("Errore durante il salvataggio CSV: " + e.getMessage());
@@ -39,15 +41,26 @@ public class SalvaCSV implements SalvaRipristinaStrategyIF {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String riga;
             while ((riga = reader.readLine()) != null) {
-                String[] campi = riga.split(",");
+                String[] campi = riga.split(",", -1);
 
-                Libro libro = new Libro.Builder(campi[0], campi[2], campi[3])
-                        .setAutoreNome(campi[1])
-                        .setGenereLibri(Genere_Libri.valueOf(campi[4]))
-                        .setValutazionePersonale(Valutazione_Personale.valueOf(campi[5]))
-                        .setStatoLettura(Stato_Lettura.valueOf(campi[6]))
-                        .build();
-                libreria.aggiungiLibro(libro);
+                Libro.Builder toBuild = new Libro.Builder(campi[0], campi[2], campi[3]);
+
+                //Ripristino e check di eventuali valori null
+                if(!campi[1].isBlank()){
+                    toBuild.setAutoreNome(campi[1]);
+                }
+                if(!campi[4].isBlank()){
+                    toBuild.setGenereLibri(Genere_Libri.valueOf(campi[4]));
+                }
+                if(!campi[5].isBlank()){
+                    toBuild.setValutazionePersonale(Valutazione_Personale.valueOf(campi[5]));
+                }
+                if(!campi[6].isBlank()){
+                    toBuild.setStatoLettura(Stato_Lettura.valueOf(campi[6]));
+                }
+
+                Libro l = toBuild.build();
+                libreria.aggiungiLibro(l);
             }
         } catch (IOException e) {
             System.err.println("Errore durante il ripristino CSV: " + e.getMessage());
